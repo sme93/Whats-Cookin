d3299b5817aaf8c2cdecddcf2074830093bc7855
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-16 15:41:10 -0400

    Implement code for recipeCollection test

--- src/classes/RecipeCollection.js
+++ src/classes/RecipeCollection.js
@@ -1,8 +1,26 @@
 class RecipeCollection {
-  constructor() {
-
-    // One class to get you started!
+  constructor(recipeData) {
+    this.recipes = [recipeData];
   }
-}
 
-export default RecipeCollection;
+  filterByTag(searchTag) {
+    const formatTag = searchTag.toLowerCase();
+    return this.recipes.filter(recipe => {
+      return recipe.tags.filter(tag => {
+        return tag.includes(formatTag)
+      })
+    })
+  }
+
+  findRecipe(searchText) {
+    const formatSearch = searchText.toLowerCase()
+    return this.recipes.filter(recipe => {
+        return recipe.ingredients.filter(ingredient => {
+          return ingredient.name.toLowerCase().includes(formatSearch) ||
+            recipe.name.toLowerCase().includes(formatSearch)
+        });
+      });
+    }
+  }
+
+  export default RecipeCollection;
