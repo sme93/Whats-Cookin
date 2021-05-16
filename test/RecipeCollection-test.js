4279760582aca4ad2494936ca3ba23ce702758aa
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-16 15:40:38 -0400

    Write tests for recipeCollection

--- test/RecipeCollection-test.js
+++ test/RecipeCollection-test.js
@@ -1,8 +1,43 @@
 import { expect } from 'chai';
+
+import { recipeData } from '../src/data/recipes.js';
+import { ingredientsData } from '../src/data/ingredients.js';
 import RecipeCollection from '../src/classes/RecipeCollection';
 
-describe('Recipe', () => {
+let recipeCollection;
+
+describe('Recipe Collection', () => {
+  beforeEach(() => {
+  recipeCollection = new RecipeCollection(recipeData);
+  });
+
   it('Should be a function', () => {
     expect(RecipeCollection).to.be.a('function');
   });
+
+  it('Should take in recipe data and store in an array', () => {
+    expect(recipeCollection.recipes).to.be.an('array')
+  });
+
+  it('Should filter recipes by one or more tags', () => {
+    expect(recipeCollection.filterByTag('snack').length).to.equal(9)
+  });
+
+  it('Should filter recipes based on an ingredient', () => {
+    expect(recipeCollection.findRecipe('eggs').length).to.equal(13);
+  });
+
+  it('Should filter recipes based on name', () => {
+   expect(recipeCollection.findRecipe('Ambrosia Cupcakes').length).to.equal(1);
+ });
+
 })
+
+
+// eggs 1123
+// A RecipeRepository should hold onto all Recipe objects.
+//
+// It should have a parameter to take in recipe data.
+// It should have methods to determine:
+// A filtered list of recipes based on one or more tags.
+// A filtered list of recipes based on its name or ingredients.
