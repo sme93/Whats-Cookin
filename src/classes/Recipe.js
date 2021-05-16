bffe88316304f2d501140e5db9d85f8c6b33e516
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-05-16 13:24:49 -0600

    Fix import exportproblems

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,7 +1,8 @@
 class Recipe {
-  constructor() {
-
+  constructor(recipe, ingredientsData) {
+    this.id = recipe.id;
   }
+
 }
 
 module.exports = Recipe;
--- test/Recipe-test.js
+++ test/Recipe-test.js
@@ -1,19 +1,27 @@
-const chai = require('chai');
-const expect = chai.expect;
+import { expect } from 'chai';
 
-const Recipe = require('../src/classes/Recipe');
-const recipeData = require('../src/data/recipes');
-const ingredientsData = require('../src/data/ingredients');
+import Recipe from '../src/classes/Recipe';
+import { recipeData } from '../src/data/recipes';
+import { ingredientsData } from '../src/data/ingredients';
 
 let recipe;
 
+
 describe('Recipe', () => {
-  beforeEach(() => {
+    beforeEach(() => {
     recipe = new Recipe(recipeData[0], ingredientsData);
   });
 
   it('should be an instance of Recipe', () => {
     expect(recipe).to.be.an.instanceOf(Recipe);
+  });
+
+  it.only('should store its id', () => {
+    expect(recipe.id).to.equal(recipeData[0].id);
+  });
+  
+  it('should store its image', () => {
+    expect(recipe.image).to.equal(recipeData[0].image);
   });
 
   it('should store its ingredients', () => {
@@ -28,20 +36,12 @@ describe('Recipe', () => {
     expect(recipe.name).to.equal(recipeData[0].name);
   });
 
-  it('should store its id', () => {
-    expect(recipe.id).to.equal(recipeData[0].id);
-  });
-
   it('should store its tags', () => {
     expect(recipe.tags).to.equal(recipeData[0].tags);
   });
 
   it('should store its ingredients data', () => {
     expect(recipe.ingredientsData).to.deep.equal(ingredientsData);
-  });
-
-  it('should store its image', () => {
-    expect(recipe.image).to.equal(recipeData[0].image);
   });
 
   it('should be able to calulate the cost of a recipe', () => {
