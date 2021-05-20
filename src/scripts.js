06660ca835cf22e7375e3b04e620e24aad503e07
rachaelcarroll <rachaelcarroll.m@gmail,com>
2021-05-19 21:45:55 -0400

    Add some pseudocode for user stories in scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -31,3 +31,28 @@ function renderRecipes(recipes) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// User Stories for RecipeCollection/Recipe 
+// As a user, I should be able to view a list of all recipes.
+// As a user, I should be able to click on a recipe to view more information including directions, ingredients needed, and total cost.
+// As a user, I should be able to filter recipes by multiple tags.
+// As a user, I should be able to search recipes by their name or ingredients.
